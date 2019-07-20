795741a9c54bb05234cee383dfd6b915d55403f5
İREM <50213878+iremaaydn@users.noreply.github.com>
2019-07-20 13:19:03 +0300

    Add files via upload

---
+++ goruntu2.m
@@ -0,0 +1,6 @@
+resim = imread("C:\Users\Aydýn\Documents\dosya1\agac.jpg");
+resim=imread("C:\Users\Aydýn\Documents\dosya1\agac.jpg");
+resim = rgb2gray(resim)
+imshow(resim)
+
+figure;
---
+++ soru2.1.m
@@ -0,0 +1,3 @@
+m1h=m1g-T-d1v
+m2h=m2g+T-d2v-R
+m3h=m3g+R-d3v
